791fc86fb41436efb4d3f0f60d9820babe768478
Gaspard Kemlin <gaspard.kemlin@enpc.fr>
2021-03-24 11:12:11 +0100

    integrate

--- Session 3 - MPI/integrate/integrate_serial.jl
+++ Session 3 - MPI/integrate/integrate_serial.jl
@@ -4,7 +4,7 @@ include("integrate.jl")
 @time begin
     a = 0.0
     b = 1.0
-    n = 2048*100000
+    n = 2048
     h = (b-a)/n
     total = trap(a, b, n, h)
     println("With $(n) trapezoids in serial, we have the following estimate : $(total)")
